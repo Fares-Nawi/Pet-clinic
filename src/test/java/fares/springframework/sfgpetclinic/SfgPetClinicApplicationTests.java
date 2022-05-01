b4e910d272b88e9a4069d4cfbbd15cc6f79988cb
Fares-Nawi <faresnaoui2@gmail.com>
2022-05-01 18:21:12 +0100

    Initial Project Add

---
+++ src/test/java/fares/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fares.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
