efd6d54d0e0daa4f6b5a3376e494d176533eefbc
Fares-Nawi <faresnaoui2@gmail.com>
2022-07-06 23:37:52 +0100

    added Specialities Map Impl . closes #31

---
+++ pet-clinic-data/src/main/java/fares/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package fares.springframework.sfgpetclinic.services;
+
+import fares.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
